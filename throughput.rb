cec38395505348477129a6bb2dc1586e867fbb4a
Winslett <cwinslett@bhm-c5-devel.harbert.net>
2008-12-05 17:22:48 -0600

    Added dynamic additions to list.

--- throughput.rb
+++ throughput.rb
@@ -8,8 +8,7 @@ port = 2000
 
 require 'socket'
 
-@routers = [Router.new(:ip_address => "10.205.5.2", :name => "bhm-rtr-wan", :interfaces => [1]),
-            Router.new(:ip_address => "10.205.5.2", :name => "bhm-rtr-wan", :interfaces => [1])]
+@routers = []
 
 Thread.new do
   loop do
@@ -25,10 +24,17 @@ TCPServer.open(port) do | s |
         while t = client.gets
           begin
             if t.strip =~ /^([^\ ]+)\ ([0-9]+)\ (in|out)$/ 
-               ip_address, interface, direction = $1, $2.to_i, $3 
-               router = @routers.find { | router | router.ip_address == ip_address }
-              raise "Could not find device by ip address" if router.nil?
-              client.puts(router.throughput(:direction => direction, :interface => interface))
+              ip_address, interface, direction = $1, $2.to_i, $3 
+              router = @routers.find { | router | router.ip_address == ip_address }
+              if router.nil?
+                @routers << Router.new(:ip_address => ip_address, :interfaces => [interface])
+              else
+                if @routers.interfaces.index(interface)
+                  client.puts(router.throughput(:direction => direction, :interface => interface))
+                else
+                  @router.interfaces << interface
+                end
+              end
             else
               raise "INVALID FORMAT: [ip_address] [interface] [direction]"
             end
